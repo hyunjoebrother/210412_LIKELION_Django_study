129ac148b489df7f8f3ffeac05c697dad167b0b5
hyunjoebrother <hyunjoe31@gmail.com>
2021-04-16 17:52:56 +0900

    210416_Django_study

--- blogproject/blog/views.py
+++ blogproject/blog/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from .models import Blog
 
 # Create your views here.
+def home(reqeust) :
+    blogs = Blog.objects # 클래스.objects : model로부터 객체 목록(Queryset)을 전달받음
+    return render(request, 'home.html', {'blogs' : blogs})
