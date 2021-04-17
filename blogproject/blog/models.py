361814e88d37c5534c71b731473dc2a570bd1905
hyunjoebrother <hyunjoe31@gmail.com>
2021-04-17 22:57:19 +0900

    210417_Django_study

--- blogproject/blog/models.py
+++ blogproject/blog/models.py
@@ -8,3 +8,4 @@ class Blog(models.Model) :
 
     def __str__(self) :
         return self.title
+        
--- blogproject/blog/views.py
+++ blogproject/blog/views.py
@@ -2,6 +2,6 @@ from django.shortcuts import render
 from .models import Blog
 
 # Create your views here.
-def home(reqeust) :
+def home(request) :
     blogs = Blog.objects # 클래스.objects : model로부터 객체 목록(Queryset)을 전달받음
     return render(request, 'home.html', {'blogs' : blogs})
