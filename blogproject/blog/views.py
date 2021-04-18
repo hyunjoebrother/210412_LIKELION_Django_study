df2f69dc3ac6d726151a92c12b37862333c3ea8e
hyunjoebrother <hyunjoe31@gmail.com>
2021-04-19 05:13:01 +0900

    210419_Django_study

--- blogproject/blog/views.py
+++ blogproject/blog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Blog
 
 # Create your views here.
@@ -8,5 +8,7 @@ def home(request) :
 
 def detail(request, blog_id) :
     # 몇 번 객체를 다룰 것인지에 대한 인자 blog_id
+    # 이용자가 원한 몇 번 블로그 객체를 사용하도록하는 함수 + import 고고
+    blog_detail = get_object_or_404(Blog, pk = blog_id)
 
     return render(request, 'detail.html', {'blog' : blog_detail})
