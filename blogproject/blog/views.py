9d2e4c822dc523365a8a7946fdfc0ad0e1f51b2b
hyunjoebrother <hyunjoe31@gmail.com>
2021-04-18 23:27:49 +0900

    210418_Django_study

--- blogproject/blog/views.py
+++ blogproject/blog/views.py
@@ -6,5 +6,7 @@ def home(request) :
     blogs = Blog.objects # 클래스.objects : model로부터 객체 목록(Queryset)을 전달받음
     return render(request, 'home.html', {'blogs' : blogs})
 
-def detail(request) :
-    return render(request, 'detail.html')
+def detail(request, blog_id) :
+    # 몇 번 객체를 다룰 것인지에 대한 인자 blog_id
+
+    return render(request, 'detail.html', {'blog' : blog_detail})
