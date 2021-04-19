258aad2bfbfe0d9588abb3efc7ce91f4474f0c14
hyunjoebrother <hyunjoe31@gmail.com>
2021-04-20 03:19:12 +0900

    210420_Django_study

--- meiblogproject/blogapp/models.py
+++ meiblogproject/blogapp/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+# App 폴더 등록하고 class 만들자
+class Blog(models.Model) :
+    title = models.CharField(max_length=200)
+    pub_date = models.DateTimeField('date published')
+    body = models.TextField()
+
+    # 작업 후 DB에 알려주는 migration 작업 ON
+    # ROCKET 확인 후 ADMIN 계정 만들고 
+    # admin.py에 import Blog
+
+    # 제목을 title로 띄우기 위해서 함수 등록
+    def __str__(self) :
+        return self.title
+
+    # 이제 model을 화면에 출력하기 위해 views.py에 import
--- meiblogproject/blogapp/views.py
+++ meiblogproject/blogapp/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
+# model을 화면에 출력하기 위해 views.py에 import
+from .models import Blog
+
 # Create your views here.
+def home(request) :
+    blogs = Blog.objects # model로부터 객체 목록을 전달
+    return render(request, 'home.html', {'blogs' : blogs})
+
+# 이제 APP 폴더 내에 templates 폴더 내에 home.html 작업
